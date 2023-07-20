0d2fae1d393f96173ff278c82f8f9353c07db131
Kamaleshwaran <124665343+Kamaleshwaran-Valarmathi@users.noreply.github.com>
2023-07-20 15:24:42 +0530

    Delete src/main/java/com/example/Wanderlust/controllers/exceptions directory

--- src/main/java/com/example/Wanderlust/controllers/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Wanderlust.controllers.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
